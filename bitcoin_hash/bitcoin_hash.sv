747ce8586ed67c05e2284d3e0456a9bb2d9495b1
Jimmy-Chu-127 <hachu@ucsd.edu>
2023-06-08 02:47:51 -0700

    fixing indentation

--- bitcoin_hash/bitcoin_hash.sv
+++ bitcoin_hash/bitcoin_hash.sv
@@ -1,9 +1,10 @@
-module bitcoin_hash (input logic        clk, reset_n, start,
-input logic [15:0] message_addr, output_addr,
-output logic        done, mem_clk, mem_we,
-output logic [15:0] mem_addr,
-output logic [31:0] mem_write_data,
-input logic [31:0] mem_read_data);
+module bitcoin_hash (
+ input logic clk, reset_n, start,
+ input logic[15:0] message_addr, output_addr,
+ output logic done, mem_clk, mem_we,
+ output logic[15:0] mem_addr,
+ output logic [31:0] mem_write_data,
+ input logic [31:0] mem_read_data);
 
 parameter num_nonces = 16;
 
